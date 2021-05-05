31cd983cc42cad276a62a01f41f024b22d8f0b9c
cmmerritt <christiane.merritt@gmail.com>
2021-05-04 17:41:53 -0700

    fix tests error, tests pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-/* import app from '../lib/app.js';
+import app from '../lib/app.js';
 import supertest from 'supertest';
 import client from '../lib/client.js';
 import { execSync } from 'child_process';
@@ -15,102 +15,110 @@ describe('API Routes', () => {
     return client.end();
   });
 
-  const expectedCats = [
+  const expectedDinos = [
     {
       id: expect.any(Number),
-      name: 'Felix',
-      type: 'Tuxedo',
-      url: 'cats/felix.png',
-      year: 1892,
-      lives: 3,
-      isSidekick: false
+      name: 'Fruitadens',
+      dinorder: 'ornithischian',
+      diet: 'herbivore',
+      region: 'North America',
+      era: 'Jurassic',
+      url: '../images/fruitadens.jpeg',
+      specimensFound: 4
     },
     {
       id: expect.any(Number),
-      name: 'Garfield',
-      type: 'Orange Tabby',
-      url: 'cats/garfield.jpeg',
-      year: 1978,
-      lives: 7,
-      isSidekick: false
+      name: 'Nanuqusaurus',
+      dinorder: 'saurischian',
+      diet: 'herbivore',
+      region: 'North America',
+      era: 'Cretaceous',
+      url: '../images/nanuqusaurus.jpeg',
+      specimensFound: 1
     },
     {
       id: expect.any(Number),
-      name: 'Duchess',
-      type: 'Angora',
-      url: 'cats/duchess.jpeg',
-      year: 1970,
-      lives: 9,
-      isSidekick: false
+      name: 'Giganotosaurus',
+      dinorder: 'saurischian',
+      diet: 'carnivore',
+      region: 'South America',
+      era: 'Cretaceous',
+      url: '../images/giganotosaurus.jpeg',
+      specimensFound: 3
     },
     {
       id: expect.any(Number),
-      name: 'Stimpy',
-      type: 'Manx',
-      url: 'cats/stimpy.jpeg',
-      year: 1990,
-      lives: 1,
-      isSidekick: true
+      name: 'Afrovenator',
+      dinorder: 'saurischian',
+      diet: 'carnivore',
+      region: 'Africa',
+      era: 'Jurassic',
+      url: '../images/afrovenator.jpeg',
+      specimensFound: 1
     },
     {
       id: expect.any(Number),
-      name: 'Sylvester',
-      type: 'Tuxedo',
-      url: 'cats/sylvester.jpeg',
-      year: 1945,
-      lives: 1,
-      isSidekick: true
+      name: 'Massospondylus',
+      dinorder: 'saurischian',
+      diet: 'herbivore',
+      region: 'Africa',
+      era: 'Jurassic',
+      url: '../images/massospondylus.jpeg',
+      specimensFound: 42
     },
     {
       id: expect.any(Number),
-      name: 'Tigger',
-      type: 'Orange Tabby',
-      url: 'cats/tigger.jpeg',
-      year: 1928,
-      lives: 8,
-      isSidekick: false
+      name: 'Australovenator',
+      dinorder: 'saurischian',
+      diet: 'carnivore',
+      region: 'Australia',
+      era: 'Cretaceous',
+      url: '../images/australovenator.jpeg',
+      specimensFound: 1
     },
     {
       id: expect.any(Number),
-      name: 'Hello Kitty',
-      type: 'Angora',
-      url: 'cats/hello-kitty.jpeg',
-      year: 1974,
-      lives: 9,
-      isSidekick: false
+      name: 'Rahonavis',
+      dinorder: 'saurischian',
+      diet: 'carnivore',
+      region: 'Madagascar',
+      era: 'Cretaceous',
+      url: '../images/rahonavis.jpeg',
+      specimensFound: 1
     },
     {
       id: expect.any(Number),
-      name: 'Hobbs',
-      type: 'Orange Tabby',
-      url: 'cats/hobbs.jpeg',
-      year: 1985,
-      lives: 6,
-      isSidekick: true
+      name: 'Alwalkeria',
+      dinorder: 'saurischian',
+      diet: 'omnivore',
+      region: 'India',
+      era: 'Triassic',
+      url: '../images/alwalkeria.jpeg',
+      specimensFound: 1
     }
   ];
 
   // If a GET request is made to /api/cats, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data?
-  it('GET /api/cats', async () => {
+  it('GET /api/dinos', async () => {
     // act - make the request
-    const response = await request.get('/api/cats');
+    const response = await request.get('/api/dinos');
 
     // was response OK (200)?
     expect(response.status).toBe(200);
 
     // did it return the data we expected?
-    expect(response.body).toEqual(expectedCats);
+    expect(response.body).toEqual(expectedDinos);
 
   });
 
   // If a GET request is made to /api/cats/:id, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data for the cat with that id?
-  test('GET /api/cats/:id', async () => {
-    const response = await request.get('/api/cats/2');
+  test('GET /api/dinos/:id', async () => {
+    const response = await request.get('/api/dinos/2');
     expect(response.status).toBe(200);
-    expect(response.body).toEqual(expectedCats[1]);
+    expect(response.body).toEqual(expectedDinos[1]);
   });
-}); */
+});
