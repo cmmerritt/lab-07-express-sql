f844039d4c7b9f3ccc81fe92d2855d7e67506ee6
cmmerritt <christiane.merritt@gmail.com>
2021-05-08 12:32:15 -0700

    pass seed data test

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import client from '../lib/client.js';
 // import our seed data:
+import users from './users.js';
 import dinos from './dinos.js';
 
 run();
@@ -9,13 +10,26 @@ async function run() {
 
   try {
 
+    const data = await Promise.all(
+      users.map(user => {
+        return client.query(`
+          INSERT INTO users (name, email, password_hash)
+          VALUES ($1, $2, $3)
+          RETURNING *;
+        `,
+        [user.name, user.email, user.password]);
+      })
+    );
+    
+    const user = data[0].rows[0];
+
     await Promise.all(
       dinos.map(dino => {
         return client.query(`
-          INSERT INTO dinos (name, dinorder, diet, region, era, url, specimens_found)
-          VALUES ($1, $2, $3, $4, $5, $6, $7);
+          INSERT INTO dinos (name, dinorder, diet, region, era, url, specimens_found, user_id)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
         `,
-        [dino.name, dino.dinorder, dino.diet, dino.region, dino.era, dino.url, dino.specimensFound]);
+        [dino.name, dino.dinorder, dino.diet, dino.region, dino.era, dino.url, dino.specimensFound, user.id]);
       })
     );
     
